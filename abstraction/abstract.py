01c0dc5e9662153d598d7e11b062e3a41c621483
nachiket0761 <nachiketdhorkule0761@gmail.com>
2026-08-15 18:16:49 +0530

    Implement abstract class A and subclass B

---
+++ abstraction/abstract.py
@@ -0,0 +1,21 @@
+
+from abc import ABC , abstractmethod
+
+class A(ABC):
+
+    def xyz(self):
+        print("hello xyz")
+
+    @abstractmethod
+    def show(self):
+        pass
+
+class B(A):
+    pass
+    def show(self):
+        print("hii i am fom show ")
+
+
+obj=B()
+obj.xyz()
+obj.show()
